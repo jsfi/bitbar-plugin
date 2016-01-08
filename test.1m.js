c571799b2b3cfb4fed40031ae9808b103be97959
Martin Sachse <martin.sachse+jsfi@gmail.com>
2016-01-08 22:26:10 +0100

    extended test

--- test.1m.js
+++ test.1m.js
@@ -1,3 +1,5 @@
-#!/usr/bin/env /Users/martinsachse/.nvm/versions/node/v5.2.0/bin/node
+#!/usr/bin/env /usr/local/bin/node
 
-console.log('Hello World');
+console.log(`Hello World | color=#f3df49`);
+console.log(`---`);
+console.log(`jsfi | color=#7dbf38 href=https://jsfi.io/`);
